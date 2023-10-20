848d5fa9769377e644f8c2d7e0daa2da282076bd
Adolpho Cavalcanti <adolpho.nascimento@climatempo.com.br>
2023-10-20 18:44:48 -0300

    feat: api rest para cpt pre 2015

--- src/plugins/climatempo/includes/admin/routes/class-api.php
+++ src/plugins/climatempo/includes/admin/routes/class-api.php
@@ -3,6 +3,7 @@
 namespace Climatempo\Admin\Routes;
 
 use Climatempo\Admin\App\Controllers\New_Notices_Controller;
+use Climatempo\Admin\App\Controllers\Notices_Pre_2015_Ct_Controller;
 use WP_REST_Server;
 
 class Api
@@ -12,6 +13,11 @@ class Api
     register_rest_route('wp/v2', '/novas-noticias-climatempo', array(
       'methods' => WP_REST_Server::READABLE,
       'callback' => [New_Notices_Controller::class, 'all_notices_climatempo'],
+    ));
+
+    register_rest_route('wp/v2', '/noticias-pre-2015-climatempo', array(
+      'methods' => WP_REST_Server::READABLE,
+      'callback' => [Notices_Pre_2015_Ct_Controller::class, 'all_notices_pre_2015_ct_climatempo'],
     ));
   }
 }
